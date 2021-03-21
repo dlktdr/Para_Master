9f7989cc30e7c2c1da197fe81ac6d55fb2932214
Cliff B <cblackburn320@gmail.com>
2021-03-21 14:40:41 -0700

    Added SBUS output support on TX pin

---
+++ para_master/src/sbus.cpp
@@ -0,0 +1,203 @@
+/*
+* Brian R Taylor
+* brian.taylor@bolderflight.com
+*
+* Copyright (c) 2021 Bolder Flight Systems Inc
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the “Software”), to
+* deal in the Software without restriction, including without limitation the
+* rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+* sell copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
+#include "sbus.h"
+
+namespace bfs {
+
+bool SbusRx::Begin() {
+  state_ = 0;
+  prev_byte_ = FOOTER_;
+  #if defined(__MK20DX128__) || defined(__MK20DX256__)
+    bus_->begin(BAUD_, SERIAL_8E1_RXINV_TXINV);
+  #else
+    bus_->begin(BAUD_, SERIAL_8N1); // ISSUE WITH ARDUINO ON BLE33, Set as 8N1, then override
+
+  #endif
+  /* flush the bus */
+  bus_->flush();
+
+  /* check communication */
+  uint32_t timer_ms = 0;
+  while (timer_ms < TIMEOUT_MS_) {
+    if (Read()) {
+      if (!failsafe() && !lost_frame()) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+bool SbusRx::Read() {
+  bool status = false;
+  /*  Parse through to the most recent packet if we've fallen behind */
+  while (bus_->available()) {
+    if (Parse()) {
+      /* Grab the channel data */
+      ch_[0]  = static_cast<uint16_t>((buf_[1]      ) | ((buf_[2]  << 8) & 0x07FF));
+      ch_[1]  = static_cast<uint16_t>((buf_[2]  >> 3) | ((buf_[3]  << 5) & 0x07FF));
+      ch_[2]  = static_cast<uint16_t>((buf_[3]  >> 6) | (buf_[4]  << 2)  |  ((buf_[5] << 10) & 0x07FF));
+      ch_[3]  = static_cast<uint16_t>((buf_[5]  >> 1) | ((buf_[6]  << 7) & 0x07FF));
+      ch_[4]  = static_cast<uint16_t>((buf_[6]  >> 4) | ((buf_[7]  << 4) & 0x07FF));
+      ch_[5]  = static_cast<uint16_t>((buf_[7]  >> 7) | (buf_[8]  << 1)  |   ((buf_[9] << 9) & 0x07FF));
+      ch_[6]  = static_cast<uint16_t>((buf_[9]  >> 2) | ((buf_[10] << 6) & 0x07FF));
+      ch_[7]  = static_cast<uint16_t>((buf_[10] >> 5) | ((buf_[11] << 3) & 0x07FF));
+      ch_[8]  = static_cast<uint16_t>((buf_[12]     ) | ((buf_[13] << 8) & 0x07FF));
+      ch_[9]  = static_cast<uint16_t>((buf_[13] >> 3) | ((buf_[14] << 5) & 0x07FF));
+      ch_[10] = static_cast<uint16_t>((buf_[14] >> 6) | (buf_[15] << 2)  |   ((buf_[16] << 10) & 0x07FF));
+      ch_[11] = static_cast<uint16_t>((buf_[16] >> 1) | ((buf_[17] << 7) & 0x07FF));
+      ch_[12] = static_cast<uint16_t>((buf_[17] >> 4) | ((buf_[18] << 4) & 0x07FF));
+      ch_[13] = static_cast<uint16_t>((buf_[18] >> 7) | (buf_[19] << 1)  |   ((buf_[20] << 9) & 0x07FF));
+      ch_[14] = static_cast<uint16_t>((buf_[20] >> 2) | ((buf_[21] << 6) & 0x07FF));
+      ch_[15] = static_cast<uint16_t>((buf_[21] >> 5) | ((buf_[22] << 3) & 0x07FF));
+      /* Channel 17 */
+      ch17_ = buf_[23] & CH17_;
+      /* Channel 18 */
+      ch18_ = buf_[23] & CH18_;
+      /* Grab the lost frame */
+      lost_frame_ = buf_[23] & LOST_FRAME_;
+      /* Grab the failsafe */
+      failsafe_ = buf_[23] & FAILSAFE_;
+      /* Set the status */
+      status = true;
+    }
+  }
+  return status;
+}
+bool SbusRx::Parse() {
+  while (bus_->available()) {
+    uint8_t c = bus_->read();
+    if (state_ == 0) {
+      if ((c == HEADER_) && ((prev_byte_ == FOOTER_) ||
+         ((prev_byte_ & 0x0F) == FOOTER2_))) {
+        buf_[state_] = c;
+        state_++;
+      } else {
+        state_ = 0;
+      }
+    } else {
+      if (state_ < LEN_) {
+        buf_[state_] = c;
+        state_++;
+      } else {
+        state_ = 0;
+        if ((buf_[LEN_ - 1] == FOOTER_) ||
+           ((buf_[LEN_ - 1] & 0x0F) == FOOTER2_)) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    }
+    prev_byte_ = c;
+  }
+  return false;
+}
+
+/* Needed for emulating two stop bytes on Teensy 3.0 and 3.1/3.2 */
+#if defined(__MK20DX128__) || defined(__MK20DX256__)
+namespace {
+  IntervalTimer serial_timer;
+  HardwareSerial *sbus_bus;
+  uint8_t sbus_packet[25];
+  volatile int send_index;
+  void SendByte() {
+    if (send_index < 25) {
+      sbus_bus->write(sbus_packet[send_index]);
+      send_index++;
+    } else {
+      serial_timer.end();
+      send_index = 0;
+    }
+  }
+}  // namespace
+#endif
+
+void SbusTx::Begin() {
+  #if defined(__MK20DX128__) || defined(__MK20DX256__)
+    bus_->begin(BAUD_, SERIAL_8E1_RXINV_TXINV);
+  #else
+    bus_->begin(BAUD_, SERIAL_8N1);
+  #endif
+}
+void SbusTx::Write() {
+  buf_[0] = HEADER_;
+  buf_[1] =  static_cast<uint8_t>((ch_[0]  & 0x07FF));
+  buf_[2] =  static_cast<uint8_t>((ch_[0]  & 0x07FF) >> 8  |
+             (ch_[1]  & 0x07FF) << 3);
+  buf_[3] =  static_cast<uint8_t>((ch_[1]  & 0x07FF) >> 5  |
+             (ch_[2]  & 0x07FF) << 6);
+  buf_[4] =  static_cast<uint8_t>((ch_[2]  & 0x07FF) >> 2);
+  buf_[5] =  static_cast<uint8_t>((ch_[2]  & 0x07FF) >> 10 |
+             (ch_[3]  & 0x07FF) << 1);
+  buf_[6] =  static_cast<uint8_t>((ch_[3]  & 0x07FF) >> 7  |
+             (ch_[4]  & 0x07FF) << 4);
+  buf_[7] =  static_cast<uint8_t>((ch_[4]  & 0x07FF) >> 4  |
+             (ch_[5]  & 0x07FF) << 7);
+  buf_[8] =  static_cast<uint8_t>((ch_[5]  & 0x07FF) >> 1);
+  buf_[9] =  static_cast<uint8_t>((ch_[5]  & 0x07FF) >> 9  |
+             (ch_[6]  & 0x07FF) << 2);
+  buf_[10] = static_cast<uint8_t>((ch_[6]  & 0x07FF) >> 6  |
+             (ch_[7]  & 0x07FF) << 5);
+  buf_[11] = static_cast<uint8_t>((ch_[7]  & 0x07FF) >> 3);
+  buf_[12] = static_cast<uint8_t>((ch_[8]  & 0x07FF));
+  buf_[13] = static_cast<uint8_t>((ch_[8]  & 0x07FF) >> 8  |
+             (ch_[9]  & 0x07FF) << 3);
+  buf_[14] = static_cast<uint8_t>((ch_[9]  & 0x07FF) >> 5  |
+             (ch_[10] & 0x07FF) << 6);
+  buf_[15] = static_cast<uint8_t>((ch_[10] & 0x07FF) >> 2);
+  buf_[16] = static_cast<uint8_t>((ch_[10] & 0x07FF) >> 10 |
+             (ch_[11] & 0x07FF) << 1);
+  buf_[17] = static_cast<uint8_t>((ch_[11] & 0x07FF) >> 7  |
+             (ch_[12] & 0x07FF) << 4);
+  buf_[18] = static_cast<uint8_t>((ch_[12] & 0x07FF) >> 4  |
+             (ch_[13] & 0x07FF) << 7);
+  buf_[19] = static_cast<uint8_t>((ch_[13] & 0x07FF) >> 1);
+  buf_[20] = static_cast<uint8_t>((ch_[13] & 0x07FF) >> 9  |
+             (ch_[14] & 0x07FF) << 2);
+  buf_[21] = static_cast<uint8_t>((ch_[14] & 0x07FF) >> 6  |
+             (ch_[15] & 0x07FF) << 5);
+  buf_[22] = static_cast<uint8_t>((ch_[15] & 0x07FF) >> 3);
+  buf_[23] = 0x00 | (ch17_ * CH17_) | (ch18_ * CH18_) |
+             (failsafe_ * FAILSAFE_) | (lost_frame_ * LOST_FRAME_);
+  buf_[24] = FOOTER_;
+
+  #if defined(__MK20DX128__) || defined(__MK20DX256__)
+    /*
+    * Use ISR to send byte at a time,
+    * 130 us between bytes to emulate 2 stop bits
+    */
+    __disable_irq();
+    memcpy(sbus_packet, tx_buffer_, sizeof(sbus_packet));
+    __enable_irq();
+    serial_timer.priority(255);
+    serial_timer.begin(SendByte, 130);
+  #else
+    bus_->write(buf_, sizeof(buf_));
+  #endif
+}
+
+}  // namespace bfs
--- para_master/src/src.ino
+++ para_master/src/src.ino
@@ -4,12 +4,16 @@
 #include "PPMIn.h"
 #include "opentxbt.h"
 #include "mbed.h"
+#include "sbus.h"
+#include <nrf_uarte.h>
 
 #define DEBUG
 #define CHANNEL_COUNT 8
 #define PPM_CENTER 1500
-#define WATCHDOG_TIMEOUT 8000
-#define IO_PERIOD 10 
+#define SBUS_CENTER 992
+#define SBUS_UPDATE_RATE 10000 // 10ms
+#define WATCHDOG_TIMEOUT 10000
+#define IO_PERIOD 10
 
 uint16_t chanoverrides = 0xFFFF;
 uint16_t ppmChannels[16];
@@ -30,9 +34,25 @@ BLECharacteristic fff6;
 BLECharacteristic butpress;
 BLECharacteristic overridech;
 Timer watchdog;
+Timer sbusupdate;
 Ticker ioTick;
 
-void setup() 
+// Create the SBUS in/out UARTS
+//UART SerialSbusTx(digitalPinToPinName(23u),digitalPinToPinName(21u),NC,NC); //
+//UART SerialSbusRx(NC,D8,NC,NC); //
+//bfs::SbusRx sbus_rx(&SerialSbusRx);
+#define UARTE0_BASE_ADDR            0x40002000  // As per nRF52840 Product spec - UARTE
+#define UART_CONFIG_REG_OFFSET      0x56C
+#define UART_BAUDRATE_REG_OFFSET    0x524 // As per nRF52840 Product spec - UARTE
+#define UART0_BAUDRATE_REGISTER     (*(( unsigned int *)(UARTE0_BASE_ADDR + UART_BAUDRATE_REG_OFFSET)))
+#define UART0_CONFIG_REGISTER       (*(( unsigned int *)(UARTE0_BASE_ADDR + UART_CONFIG_REG_OFFSET)))
+#define BAUD100000                   0x0198EF80
+#define CONF8E2                      0x0000001E
+
+bfs::SbusTx sbus_tx(&Serial1);
+std::array<uint16_t, 16> sbus_data;
+
+void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
     pinMode(LED_BLUE, OUTPUT);
@@ -41,23 +61,32 @@ void setup()
     digitalWrite(LED_BLUE,HIGH); // Connected If Blue
 
     Serial.begin(115200);
-    
+
+    // Start SBUSOut on Pin D8
+    sbus_tx.Begin();
+
+    // Issue with Arduino, Cannot set 8E2 without crashing mbed.
+    // this manually sets the config regs to 100000 baud 8E2
+    UART0_BAUDRATE_REGISTER = BAUD100000;
+    UART0_CONFIG_REGISTER = CONF8E2;
+
     if(!BLE.begin()) {
         Serial.println("Could not start BLE");
         while(1);
     }
-        
-    Serial.print("Local BLE Address: "); 
+
+    Serial.print("Local BLE Address: ");
     Serial.println(BLE.address());
 
     PpmOut_setChnCount(CHANNEL_COUNT);
     PpmOut_setPin(D10);
-    
+
     PpmIn_setPin(D9);
 
     // Default all CH's at center
     for(int i=0;i < CHANNEL_COUNT; i++) {
         ppmChannels[i] = PPM_CENTER;
+        sbus_data[i] = SBUS_CENTER;
     }
 
     // Start the IO task at 100hz interrupt
@@ -65,23 +94,24 @@ void setup()
 
     // Start watchdog timer
     watchdog.start();
+    sbusupdate.start();
 }
 
 // Connected and data being sent
 bool bleconnected = false;
 
-void loop() 
-{     
+void loop()
+{
     // Read the PPM input data
     uint16_t ppmin[16];
     PpmIn_execute();
     ppminchcnt = PpmIn_getChannels(ppmin);
 
     // PPM Data Received
-    if(ppminchcnt >= 4 && ppminchcnt <= CHANNEL_COUNT) {                
+    if(ppminchcnt >= 4 && ppminchcnt <= CHANNEL_COUNT) {
         // If not connected push all PPM's to output
         if(!bleconnected) {
-            for(int i=0;i < ppminchcnt; i++) 
+            for(int i=0;i < ppminchcnt; i++)
                 ppmChannels[i] = ppmin[i];
 
         // If connected only push the PPM's that aren't overriden
@@ -99,7 +129,7 @@ void loop()
         if(!bleconnected) {
             for(int i=0;i < CHANNEL_COUNT; i++)
                 ppmChannels[i] = PPM_CENTER;
-        
+
         // If connected, set all non-overrides to center
         } else {
             for(int i=0;i < CHANNEL_COUNT; i++) {
@@ -116,28 +146,43 @@ void loop()
         PpmOut_setChannel(i,ppmChannels[i]);
     }
 
+    // Set and send the SBUS data
+    for(int i=0;i < CHANNEL_COUNT; i++) {
+        sbus_data[i] = ppmChannels[i] - (PPM_CENTER - SBUS_CENTER);
+    }
+
+    sbus_tx.tx_channels(sbus_data);
+    sbus_tx.failsafe(false);
+    sbus_tx.lost_frame(false);
+    sbus_tx.ch17(false);
+    sbus_tx.ch18(false);
+    if(sbusupdate.read_high_resolution_us() > SBUS_UPDATE_RATE) {
+        sbusupdate.reset();
+        sbus_tx.Write();
+    }
+
     // Reset to center from this slave board
     if(bleconnected && butpress) {
-        if(digitalRead(D2) == 0 || 
+        if(digitalRead(D2) == 0 ||
            digitalRead(D3) == 0) {
             butpress.writeValue((uint8_t)'R');
         } else {
             butpress.writeValue((uint8_t)0);
-        }    
+        }
     }
 
     // Start Scan for PARA Slaves
     if(!BLE.connected() && !scanning) {
         Serial.println("Starting Scan");
         BLE.scan();
-        scanning = true;           
+        scanning = true;
         bleconnected = false;
     }
 
     // If scanning see if there is a BLE device available
     if(!BLE.connected() && scanning) {
         bool fault = false;
-        peripheral = BLE.available(); 
+        peripheral = BLE.available();
         if(peripheral) {
             if (peripheral.address() == "") {
                 Serial.print(" <no advertised address> ");
@@ -166,14 +211,14 @@ void loop()
                 Serial.println("Stopping scan");
                 BLE.stopScan();
                 scanning = false;
-                Serial.println("Connecting...");       
+                Serial.println("Connecting...");
                 if(peripheral.connect()) {
                     Serial.println("Connected");
                     delay(100); // Attribute discovery fails if done right away
                     Serial.println("Discovering Attributes");
                     if(peripheral.discoverAttributes()) {
-                        Serial.println("Discovered Attributes"); 
-                        fff6 = peripheral.service("fff0").characteristic("fff6");                        
+                        Serial.println("Discovered Attributes");
+                        fff6 = peripheral.service("fff0").characteristic("fff6");
                         if(fff6) {
                             Serial.println("Attaching Event Handler");
                             fff6.setEventHandler(BLEWritten, fff6Written);  // Call this function on data received
@@ -181,14 +226,14 @@ void loop()
                             Serial.println("Subscribing...");
                             delay(100); // Doesn't always send data if done right away
                             if(fff6.subscribe()) {
-                                Serial.println("Subscribed to data!");                                
+                                Serial.println("Subscribed to data!");
                             } else {
                                 Serial.println("Subscribe tp data failed");
                                 fault = true;
                             }
                         } else  {
                             Serial.println("Couldn't find characteristic");
-                            fault = true;                        
+                            fault = true;
                         }
                         // If this is a Headtracker it may have a reset center option
                         butpress = peripheral.service("fff1").characteristic("fff2");
@@ -196,23 +241,23 @@ void loop()
                             Serial.println("Tracker has ability to remote reset center");
                         }
                         overridech = peripheral.service("fff1").characteristic("fff1");
-                        if(overridech) {    
+                        if(overridech) {
                             overridech.setEventHandler(BLEWritten, overrideWritten);  // Call this function on data received
                             // Initial read of overridden channels
-                            overridech.readValue(chanoverrides);                
+                            overridech.readValue(chanoverrides);
                             Serial.println("Tracker has the channels it wants overriden");
                             delay(100); // Doesn't always send data if done right away
                             if(overridech.subscribe()) {
-                                Serial.println("Subscribed to channel overrides!");                                
+                                Serial.println("Subscribed to channel overrides!");
                             } else {
                                 Serial.println("Subscribe to override Failed");
                                 fault = true;
                             }
-                        } else 
+                        } else
                             chanoverrides = 0xFFFF;
                     } else {
                         Serial.println("Attribute Discovery Failed");
-                        fault = true;                    
+                        fault = true;
                         }
                 } else {
                     Serial.println("Couldn't connect to Para Slave, Rescanning");
@@ -238,7 +283,7 @@ void loop()
             Serial.println("***WATCHDOG.. Forcing disconnect. No data received");
             BLE.disconnect();
             bleconnected = false;
-        }        
+        }
 
     // Not Connected
     } else {
@@ -268,7 +313,7 @@ void fff6Written(BLEDevice central, BLECharacteristic characteristic) {
 
     // Got some data clear the watchdog timer
     watchdog.reset();
-  
+
     uint8_t buffer1[BLUETOOTH_LINE_LENGTH+1];
     int len = characteristic.readValue(buffer1,32);
 
@@ -276,16 +321,16 @@ void fff6Written(BLEDevice central, BLECharacteristic characteristic) {
     for(int i=0;i<len;i++) {
         processTrainerByte(buffer1[i]);
     }
-  
+
     // Got the Channel Data, Set PPM Output if override bits set
     for(int i=0;i<CHANNEL_COUNT;i++) {
-        // Only set the data on channels that are allowed to be overriden        
+        // Only set the data on channels that are allowed to be overriden
         if(chanoverrides & (1<<i)) {
             ppmChannels[i] = ppmInput[i];
         }
-    }    
+    }
 
-#ifdef DEBUG       
+#ifdef DEBUG
     Serial.print("OR: ");
     printHex(chanoverrides);
     Serial.print("|");
@@ -293,7 +338,7 @@ void fff6Written(BLEDevice central, BLECharacteristic characteristic) {
         Serial.print("Ch");Serial.print(i+1);Serial.print(":");Serial.print(ppmChannels[i]);Serial.print(" ");
     }
     Serial.println("");
-#endif        
+#endif
 }
 
 // Any IO Related Tasks, buttons, etc.. ISR. Run at 1Khz
@@ -303,7 +348,7 @@ void io_Task()
   // Fast Blink to know it's running
   if(i==10) {
     digitalWrite(LED_BUILTIN, HIGH);
-  } 
+  }
   if(i==20) {
     digitalWrite(LED_BUILTIN, LOW);
     i=0;
